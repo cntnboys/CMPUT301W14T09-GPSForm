d1af8d6a8ef9a16bb3a9cc6aa43f912f38a018e2
mcmorris <mcmorris@ualberta.ca>
2014-03-07 12:10:11 -0700

    Update Thread.java
    added length field.

--- CMPUT301W14T09/src/ca/cmput301w14t09/model/Comment/Thread.java
+++ CMPUT301W14T09/src/ca/cmput301w14t09/model/Comment/Thread.java
@@ -4,10 +4,12 @@ public class Thread {
 	private LinkedList comments;
 	private DateTime lastUpdated;
 	private String name;
+	private int length;
 
 	public Thread() {
 		comments = new LinkedList();
 		name = "";
+		length = 0;
 	}
 	
 	public void addToThread(Comment comment) {
@@ -18,8 +20,10 @@ public class Thread {
 		// Iterate through and append to tail.
 		ListIterator cursor = comments.findTail();
 		comments.insertAfter(comment, cursor);
+		length = length + 1;
 	}
 	
 	public LinkedList getComments() { return comments; }
 	public String getName() { return name; }
+	public int getLength() { return length; }
 }
