c56b16c2fea83d8f64138495047479e4ef089a00
HooHoo33 <cntnboys@hotmail.com>
2015-01-24 00:22:57 -0700

    Update R.java

--- CMPUT301W14T09/gen/ca/cmput301w14t09/R.java
+++ CMPUT301W14T09/gen/ca/cmput301w14t09/R.java
@@ -11,9 +11,5 @@ public final class R {
     public s
     }
     public static final class layout {
-        public static final int activity_comment_list=0x7f030000;
-  
-        Base application theme for API 11+. This theme completely replaces
-        Appnt MyActionButtonOverflow=0x7f060002;
-    }
+        p
 }
